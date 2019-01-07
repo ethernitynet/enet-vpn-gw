ae66086d06bb5bba5a0712cf788f88be83f8b072
Yotam Ramon <yotam@ethernitynet.com>
2019-01-07 08:07:37 -0500

    add placeholder for vpn gw boot

--- runtime/utils/enet_vpn_gw_runtime.sh
+++ runtime/utils/enet_vpn_gw_runtime.sh
@@ -9,7 +9,7 @@ enet_vpn_gw_start() {
 	enet_ovs_attach_nic_br ${ENET_NIC_BR} ${OVS_VPN_BR} ${ENET_NIC_INTERFACE} ${ENET_NIC_PCI}
 }
 
-enet_vpn_run_libreswan() {
+enet_vpn_boot_libreswan() {
 
-	echo 'enet_vpn_run_libreswan'
+	ovs_dpdk add-docker-port br0 lib107 libreswan --ipaddress=15.1.1.108/24 --macaddress=00:62:B1:49:1D:BF
 }
