e3641e817007fb871b8b0258c111d756b966cd50
root <erezb@ethernitynet.com>
2019-02-11 08:25:16 -0500

    s2n traffic

--- runtime/bench/trex/trx_pcap.sh
+++ runtime/bench/trex/trx_pcap.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
 
-PY_EXE=/home/trx/v2.51/automation/trex_control_plane/interactive/trex/examples/stl/stl_pcap.py
+if false
+then
 PCAP_FILE=/root/ETHERNITY/GITHUB/enet-vpn-gw/runtime/bench/multinic/n2s_conn0_800.pcap
 TX_PORT=1
-INTERVAL_USEC=80
+else
+PCAP_FILE=/root/ETHERNITY/GITHUB/enet-vpn-gw/runtime/bench/multinic/s2n_conn0_800.pcap
+TX_PORT=2
+fi
+PY_EXE=/home/trx/v2.51/automation/trex_control_plane/interactive/trex/examples/stl/stl_pcap.py
+INTERVAL_USEC=3
 
 python ${PY_EXE} -f ${PCAP_FILE} -p ${TX_PORT} -n 0 -i ${INTERVAL_USEC}
 
