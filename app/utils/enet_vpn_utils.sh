710169bf1ed0f106ea9d52f6cee1e3b1d6fb7802
root <erezb@ethernitynet.com>
2019-03-05 06:53:48 -0500

    verify updated nodejs version before npm install

--- app/utils/enet_vpn_utils.sh
+++ app/utils/enet_vpn_utils.sh
@@ -6,6 +6,9 @@ enet_vpn_config_mngr_install() {
 	ln -sf ${SRC_DIR}/schema/asf/bower_components/angular-schema-form-bootstrap/examples/data ${SRC_DIR}/schema/asf/ng/data
 	ln -sf ${SRC_DIR}/runtime/bench ${SRC_DIR}/schema/asf/ng/model/bench
 	cd ${SRC_DIR}/config
+	apt-get -y update && apt-get install -y gnupg2 curl
+	curl -sL https://deb.nodesource.com/setup_10.x | sudo bash -
+	apt-get -y install nodejs
 	npm install
 	cd -
 	cd ${ENET_VPN_BACKEND_DIR}
